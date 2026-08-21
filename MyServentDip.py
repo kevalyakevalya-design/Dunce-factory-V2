e1732f0e48397c8adf33ad4476504e14d6a011ee
kevalya <kevalya.kevalya@torontomu.ca>
2026-08-21 16:09:55 -0400

    Complete chapter 2 and start embeddings

--- MyServentDip.py
+++ MyServentDip.py
@@ -124,4 +124,119 @@ text = (
 )
 integers = tokenizer.encode(text,allowed_special = {"<|endoftext|>"})
 print(integers)
+strings = tokenizer.decode(integers)
+print(strings)
+#now the dunceulator is able to break down words perchance
+with open("the-verdict.txt", "r", encoding = "utf-8") as f:
+    raw_text = f.read()
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+enc_sample = enc_text[50:]
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+print(f"x: {x}")
+print(f"y:    {y}")
+#the x and y cooridinates help out with the arrays
+
+for i in range(1, context_size+1):
+    context = enc_sample[:1]
+    desired = enc_sample[1]
+
+    print(context, "-->",)
+for i in range(1, context_size+1):
+    context = enc_sample[:1]
+    desired = enc_sample[1]
+    print(tokenizer.decode(context),"-->",tokenizer.decode([desired]))
+
+#importing torch
+import torch
+
+from torch.utils.data import dataset, dataloader
+class GPTDatasetV1(dataset):
+    def _init_(self, txt, tokenizer, maxlength,stride):
+        self.input_ids = []
+        self.target_ids = []
+        #tokenizer the entire t
+        token_ids = tokenizer.encode(text, allowed_special ={"<|endoftext|>"})
+        assert len(token_ids)> max_length, "number token imputs must equal +1"
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i+1: i + max_length+1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+    def _len_(self):
+        return len(self.input_ids)
+    def _getitem_(self,idx):
+        return self.input_ids[idx], self.target_ids[idx]
+def creat_dataloader_v1(
+    txt,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    dataset = GPTDatasetV1(
+        txt,
+        tokenizer,
+        max_length,
+        stride
+    )
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
+with open("the - verdict.txt","r", encoding = "utf-8") as f:
+    raw_text = f.read()
+dataloader = creat_dataloader_v1(
+    raw_text, batch_size = 1, maxleangth = 4, stride = 1, shuffe= False
+) 
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
+
+second_batch= next(data_iter)
+print(second_batch)
+
+dataloader = creat_dataloader_v1(raw_text, batch_size=8, max_length = 4, stride = 4, stride = 4, shuffle = False)
+
+data_iter = iter(dataloader)
+input, targets = next(data_iter)
+print("inputs\n", input)
+print("\nTargets:\n", targets)
+input_ids = torch.tensor([2, 3, 5, 1])
+vocab_size = 6
+output_dim = 3
+torch.manual_seed(123)
+embedding_layer = torch.nn.embedding(vocab_size,output_dim)
+print(embedding_layer.weight)
+print(embedding_layer.weight)
+print(embedding_layer(torch.tensor([3])))
+print(embedding_layer(input_ids))
+max_length = 4
+dataloader = creat_dataloader_v1(
+    raw_text, batch_size=8, max_length= max_length,
+    stride= max_length, shuffle = False
+
+)
+data_iter = iter(dataloader)
+input, targets - next(data_iter)
+print("token IDs:\n", input)
+print("\nInputs shape\n", input.shape)
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(max_length))
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
+
 
