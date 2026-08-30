9dde5e3332176768bd410de2366940f758100195
kevalya <kevalya.kevalya@torontomu.ca>
2026-08-30 13:20:51 -0400

    Chapter 4 progress

--- myserventdipV3.py
+++ myserventdipV3.py
@@ -775,3 +775,36 @@ class GELU(nn.module):
             (x + 0.044715 * torch.pow( x ,3))
         ))
 
+import matplotlib.pyplot as plt
+gelu, relu = Gelu(), nn.Relu()
+
+# put data heer
+
+x = torch.linspace(-3, 3, 100)
+y_gelu, y_relu = gelu(x), relu(x)
+plt.figure(figsize)
+for i, (y, label) in enumerate(zip([y_gelu, y_relu],["GELU, ReLU"]), 1):
+    plt.subplot(1, 2, i)
+    plt.plot(x, y)
+    plt.title(f"{lable} activation funtion")
+    plt.xlabel(f"x")
+    plt.ylabel(f"{lable}(x)")
+    plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+class FeedForward(nn.Module):
+    def __init__(self,cfg):
+        super(). __init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4* cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+
+        )
+
+    def forward(self, x)
+        return self.layers(x)
+
+print(GPT_CONFIG_124M["emb_dim"])
