fa2ac18e45bd9c71f7dd0b4e94cc91c2e2cb6d82
kevalya <kevalya.kevalya@torontomu.ca>
2026-09-01 13:29:33 -0400

    Chapter 4 progress

--- myserventdipV3.py
+++ myserventdipV3.py
@@ -951,3 +951,51 @@ out = modle(bathc)
 print("Input batch:\n", batch)
 print("\nOutput shape:", out.shape)
 print(out)
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params:,}")
+
+print("Token embedding layer shape:", model.tok_emb.weight.shape)
+print("Output layer shape:", model.out_head.weight.shape)
+
+total_params_gpt2 =  total_params - sum(p.numel() for p in model.out_head.parameters())
+print(f"Number of trainable parameters considering weight tying: {total_params_gpt2:,}")
+
+# Calculate the total size in bytes (assuming float32, 4 bytes per parameter)
+
+total_size_bytes = total_params * 4
+
+# Convert to megabytes
+
+total_size_mb = total_size_bytes / (1024 * 1024)
+
+print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (batch, n_tokens) array of indices in the current context
+    for _ in range(max_new_tokens):
+        
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+        
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+        
+        # Focus only on the last time step
+        # (batch, n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]  
+
+        # Apply softmax to get probabilities
+        probas = torch.softmax(logits, dim=-1)  # (batch, vocab_size)
+
+        # Get the idx of the vocab entry with the highest probability value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
