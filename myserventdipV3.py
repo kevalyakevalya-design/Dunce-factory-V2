a88d84d74e6ee9b4ab48f91df274d47f23e5813c
kevalya <kevalya.kevalya@torontomu.ca>
2026-09-09 15:53:27 -0400

    Chapter 4 progress

--- myserventdipV3.py
+++ myserventdipV3.py
@@ -1751,3 +1751,25 @@ token_ids = generate_text_simple(
 )
 
 print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
+
+with torch.no_grad():
+    logits = model(inputs)
+
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size)
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+print("Token IDs:\n", token_ids)
+print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
+print(f"Outputs batch 1: {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
